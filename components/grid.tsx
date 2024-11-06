d69da7d844f92441a6a5e8604916222e00f9dd5b
Davi Rezende <daviresendes12@gmail.com>
2024-11-05 22:31:12 -0300

    feat: first 3 cards

---
+++ components/grid.tsx
@@ -0,0 +1,35 @@
+import { gridItems } from '@/data'
+import { BentoGrid, BentoGridItem } from './ui/bento-grid'
+
+export function Grid() {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              title={title}
+              description={description}
+              id={id}
+              key={id}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          ),
+        )}
+      </BentoGrid>
+    </section>
+  )
+}
